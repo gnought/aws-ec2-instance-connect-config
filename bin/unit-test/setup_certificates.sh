2b8825d313e49f03977beeb115a24b184c04e121
gnought <1684105+gnought@users.noreply.github.com>
2026-03-21 01:39:30 +0800

    fix(test): compatible wither newer OpenSSL

--- bin/unit-test/setup_certificates.sh
+++ bin/unit-test/setup_certificates.sh
@@ -78,7 +78,8 @@ extendedKeyUsage = OCSPSigning
 [v3_ca]
 subjectKeyIdentifier=hash
 authorityKeyIdentifier=keyid:always,issuer:always
-basicConstraints = CA:TRUE
+basicConstraints = critical, CA:TRUE
+keyUsage = critical, cRLSign, digitalSignature, keyCertSign
 
 [req]
 distinguished_name = req_distinguished_name
@@ -91,7 +92,7 @@ sed -i "s|REPLACE_WITH_CERTPATH|${certpath}|" "${certpath}/ca.conf"
 
 # Generate the CA
 "${OPENSSL}" genrsa -out "${certpath}/ca.key" 2048 > /dev/null 2>&1
-"${OPENSSL}" req -x509 -new -nodes -key "${certpath}/ca.key" -sha256 -days 1 -out "${certpath}/ca.crt" -subj "/CN=managedssh.amazonaws.com" > /dev/null 2>&1
+"${OPENSSL}" req -x509 -new -nodes -config "${certpath}/ca.conf" -key "${certpath}/ca.key" -sha256 -days 1 -out "${certpath}/ca.crt" -extensions v3_ca -subj "/CN=managedssh.amazonaws.com" > /dev/null 2>&1
 "${OPENSSL}" x509 -in "${certpath}/ca.crt" -outform PEM -out "${certpath}/ca.pem"
 subject=$("${OPENSSL}" x509 -noout -subject -in "${certpath}/ca.pem" | sed -n -e 's/^.*CN=//p')
 # Add "# subject" to start
